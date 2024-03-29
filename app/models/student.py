c0a82e9e18ce1507c815e10a5b9cd1c5e68b14af
dat.ngoquang <dat.ngoquang@glinteco.com>
2024-03-29 21:51:52 +0700

    Add model databse

---
+++ app/models/student.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from .base import BaseModel
+
+
+class Student(BaseModel):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="students"
+    )
+    avatar = models.FileField(upload_to="avatar/", blank=True, null=True)
+    skills = models.ManyToManyField(
+        "Skill",
+        related_name="student_skills",
+        blank=True,
+    )
+    hobbies = models.ManyToManyField(
+        "Hobby",
+        related_name="student_hobbies",
+        blank=True,
+    )
+    classroom = models.CharField(default="", max_length=255, blank=True)
+    fb_link = models.URLField(default="", blank=True, max_length=255)
+    github_link = models.URLField(default="", blank=True, max_length=255)
+    instagram_link = models.URLField(default="", blank=True, max_length=255)
+    phone = models.CharField(default="", blank=True, max_length=255)
+    address = models.URLField(default="", blank=True, max_length=255)
+
+    def full_name(self):
+        return f"{self.user.first_name} {self.user.last_name}"
