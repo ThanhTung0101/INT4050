a8a3d1e993f5e6d0cba2cfe5b8552c756526ad5b
ThanhTung0101 <20020071@vnu.edu.vn>
2024-05-08 02:41:26 +0700

    update

--- app/views/document/detail.py
+++ app/views/document/detail.py
@@ -4,7 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.contenttypes.models import ContentType
 from django.views.generic import DetailView
 
-from app.models import Comment, Document
+from app.models import Comment, Document, Like
 from libs import BaseViewMixin
 
 
@@ -20,4 +20,13 @@ class DocumentDetailView(LoginRequiredMixin, BaseViewMixin, DetailView):
             content_type=content_type,
             object_id=document.pk,
         ).order_by("-created_at")
+        context["is_like"] = Like.objects.filter(
+            content_type=content_type,
+            object_id=document.pk,
+            owner=self.request.user.student,
+        )
+        context["like_count"] = Like.objects.filter(
+            content_type=content_type,
+            object_id=document.pk,
+        ).count()
         return context
